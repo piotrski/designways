171e3814ef081b0b76b1f02a1068d051ea5b776e
Tomasz Osowski <osowski.tom@gmail.com>
2021-01-30 13:37:22 +0100

    structrue

--- filter.js
+++ filter.js
@@ -1,117 +1,122 @@
-const eventList = [
-  {
-    title: "Ekonomia behawioralna w UX",
-    url: "https://uxupgrade.pl/szkolenie/ekonomia-behawioralna",
-    img:
-      "https://dl.airtable.com/.attachments/98e565f9ebb08facb055902d76013e81/1ce22c0b/Projektowanieperswazyjne.jpg",
-    type: "szkolenie / warsztat",
-    location: "Online",
-    speaker: "Piotr Kozłowski",
-    spots: 12,
-    date: "4.2.2021 09:30",
-    "Zakończenie wydarzenia": "4.2.2021 13:30",
-    duration: "0:04",
-    level: "Junior",
-    tags: ["UX", "Product Design"],
-    language: "polski",
-    certification: "Tak",
-    Status: "Czeka na dodanie",
-    Platne: true,
-    price: 613.8,
-  },
-  {
-    title: "Ekonomia behawioralna w UX",
-    url: "https://designthinkingkurs.pl/",
-    img: "/images/image-thumbnail.jpg",
-    language: "polski",
-    testimonials: "",
-    speaker: "Sara Kapela",
-    location: "Online",
-    type: "szkolenia/warsztat",
-    date: "8.02.2021",
-    duration: "32h",
-    spots: "15",
-    level: "Początkujący",
-    UX: true,
-    tags: ["UX"],
-    Darmowe: true,
-    certification: true,
-    price: "Darmowe",
-  },
-  {
-    title: "Ekonomia behawioralna w UX platne",
-    url: "https://designthinkingkurs.pl/",
-    img: "/images/image-thumbnail.jpg",
-    language: "polski",
-    testimonials: "",
-    prowadzacy: "Sara Kapela",
-    location: "Online",
-    type: "szkolenia/warsztat",
-    date: "8.02.2021",
-    duration: "32h",
-    spots: "15",
-    level: "Początkujący",
-    UX: true,
-    productDesign: true,
-    UI: true,
-    tags: ["UX", "Product Design", "UI"],
-    Platne: true,
-    certification: true,
-    price: "613,8zł",
-  },
-];
-window.addEventListener("DOMContentLoaded", (event) => {
-  let filterClick = [...document.querySelectorAll(".tagsFilter")];
-  let filterArray = {};
-  console.log(filterArray);
-  for (const key of filterClick) {
-    filterArray[key.dataset.key] = null;
-  }
+(() => {
+  const eventList = [
+    {
+      title: "Ekonomia behawioralna w UX",
+      url: "https://uxupgrade.pl/szkolenie/ekonomia-behawioralna",
+      img:
+        "https://dl.airtable.com/.attachments/98e565f9ebb08facb055902d76013e81/1ce22c0b/Projektowanieperswazyjne.jpg",
+      type: "szkolenie / warsztat",
+      location: "Online",
+      speaker: "Piotr Kozłowski",
+      spots: 12,
+      date: "4.2.2021 09:30",
+      "Zakończenie wydarzenia": "4.2.2021 13:30",
+      duration: "0:04",
+      level: "Junior",
+      Poczatkujacy: true,
+      tags: ["UX", "Product Design"],
+      language: "polski",
+      certification: "Tak",
+      Status: "Czeka na dodanie",
+      Platne: true,
+      price: 613.8,
+    },
+    {
+      title: "Ekonomia behawioralna w UX",
+      url: "https://designthinkingkurs.pl/",
+      img: "/images/image-thumbnail.jpg",
+      language: "polski",
+      testimonials: "",
+      speaker: "Sara Kapela",
+      location: "Online",
+      type: "szkolenia/warsztat",
+      date: "8.02.2021",
+      duration: "32h",
+      spots: "15",
+      level: "Początkujący",
+      Poczatkujacy: true,
+      tags: ["UX"],
+      UX: true,
+      Darmowe: true,
+      certification: true,
+      price: "Darmowe",
+    },
+    {
+      title: "Ekonomia behawioralna w UX platne",
+      url: "https://designthinkingkurs.pl/",
+      img: "/images/image-thumbnail.jpg",
+      language: "polski",
+      testimonials: "",
+      prowadzacy: "Sara Kapela",
+      location: "Online",
+      type: "szkolenia/warsztat",
+      date: "8.02.2021",
+      duration: "32h",
+      spots: "15",
+      level: "Początkujący",
+      Poczatkujacy: true,
+      UX: true,
+      productDesign: true,
+      UI: true,
+      tags: ["UX", "Product Design", "UI"],
+      Platne: true,
+      certification: true,
+      price: "613,8zł",
+    },
+  ];
 
-  const filterElement = (e) => {
-    e.target.classList.value.includes("tag__single--active")
-      ? e.target.classList.remove("tag__single--active")
-      : e.target.classList.add("tag__single--active");
-
-    const clickedElement = e.target.dataset.key.toString();
-    let FilteredList;
-
-    filterArray[`${clickedElement}`] == null
-      ? (filterArray[`${clickedElement}`] = true)
-      : (filterArray[`${clickedElement}`] = null);
-
+  window.addEventListener("DOMContentLoaded", (event) => {
+    let filterClick = [...document.querySelectorAll(".tagsFilter")];
+    let filterArray = {};
     console.log(filterArray);
+    for (const key of filterClick) {
+      filterArray[key.dataset.key] = null;
+    }
 
-    const filterItem = (item) => {
-      for (const property in filterArray) {
-        if (item[`${property}`] === filterArray[`${property}`]) {
-          return true;
+    const filterElement = (e) => {
+      e.target.classList.value.includes("tag__single--active")
+        ? e.target.classList.remove("tag__single--active")
+        : e.target.classList.add("tag__single--active");
+
+      const clickedElement = e.target.dataset.key.toString();
+      let FilteredList;
+
+      filterArray[`${clickedElement}`] == null
+        ? (filterArray[`${clickedElement}`] = true)
+        : (filterArray[`${clickedElement}`] = null);
+
+      console.log(filterArray);
+
+      const filterItem = (item) => {
+        for (const property in filterArray) {
+          if (item[`${property}`] === filterArray[`${property}`]) {
+            return true;
+          }
         }
-      }
-      return false;
+        return false;
+      };
+
+      FilteredList = eventList.filter(filterItem);
+
+      checkWhatToDisplay(FilteredList);
     };
 
-    FilteredList = eventList.filter(filterItem);
+    const checkWhatToDisplay = (FilteredList) => {
+      if (Object.values(filterArray).indexOf(true) > -1) {
+        renderElement(FilteredList);
+      } else {
+        renderElement(eventList);
+      }
+    };
 
-    checkWhatToDisplay(FilteredList);
-  };
-
-  const checkWhatToDisplay = (FilteredList) => {
-    if (Object.values(filterArray).indexOf(true) > -1) {
-      renderElement(FilteredList);
-    } else {
-      renderElement(eventList);
-    }
-  };
-
-  const renderElement = (arrayList) => {
-    let contenerList = ``;
-    arrayList.map((workshop) => {
-      let tags = "";
-      workshop.tags.map((tag) => {
-        tags += `<div class="tag__single tag__single--small">${tag}</div>`;
-      });
-      const currentHtml = `
+    const renderElement = (arrayList) => {
+      let contenerList = ``;
+      arrayList.map((workshop) => {
+        let tags = "";
+        workshop.tags.map((tag) => {
+          tags += `<div class="tag__single tag__single--small">${tag}</div>`;
+        });
+        const currentHtml = `
       <div class="lecture">
        <div class="lecture__half">
          <img class="lecture__image" src="${workshop.img}" alt="" />
@@ -151,16 +156,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
          <div class="lecture__rightGrid__price">${workshop.price}</div>
        </div>
      </div>`;
-      tags = "";
-      contenerList += currentHtml;
+        tags = "";
+        contenerList += currentHtml;
+      });
+      let signle = document.getElementById("lecture");
+      signle.innerHTML = contenerList;
+    };
+
+    renderElement(eventList);
+
+    filterClick.forEach((element) => {
+      element.addEventListener("click", filterElement);
     });
-    let signle = document.getElementById("lecture");
-    signle.innerHTML = contenerList;
-  };
-
-  renderElement(eventList);
-
-  filterClick.forEach((element) => {
-    element.addEventListener("click", filterElement);
   });
-});
+})();
