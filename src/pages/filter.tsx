52568fb29c72f1ee62990c4046762848cce24078
Marek Mitis <marek.mitis@revolut.com>
2021-03-06 18:15:14 +0100

    Try

--- src/pages/filter.tsx
+++ src/pages/filter.tsx
@@ -3,6 +3,8 @@ import { Head } from "../components/Head";
 import { Footer } from "../components/Footer";
 import { AuthChecker } from "../components/AuthChecker";
 
+console.log('dsad')
+
 export default function Filter() {
   return (
     <AuthChecker>
