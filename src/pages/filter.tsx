8942f11901876a95dc7b6d06534a3170861eb280
Marek Mitis <marek.mitis@revolut.com>
2021-03-06 18:31:32 +0100

    fix filters lint

--- src/pages/filter.tsx
+++ src/pages/filter.tsx
@@ -1,9 +1,7 @@
-import React from "react"
+import React from "react";
 import { Head } from "../components/Head";
 import { Footer } from "../components/Footer";
 import { AuthChecker } from "../components/AuthChecker";
-
-console.log('dsad')
 
 export default function Filter() {
   return (
